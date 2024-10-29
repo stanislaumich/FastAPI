727efa1efaf70be487533e4065541cc6e81d3634
stanislaumich <staslz42stas.com>
2024-10-29 23:33:18 +0300

    AUTO FROM HOME 29.10.2024 23:33:17,12

--- P1/main.py
+++ P1/main.py
@@ -3,12 +3,14 @@ from fastapi import FastAPI
 from fastapi.middleware.cors import CORSMiddleware
 from database import SessionLocal, engine, Base
 from routers import user as UserRouter
+from fastapi.staticfiles import StaticFiles
 
 Base.metadata.create_all(bind=engine)
 
 app = FastAPI()
 origins = ["*"]
-
+#app.mount("/static", StaticFiles(directory="static", html=True))
+app.mount("/", StaticFiles(directory="static", html=True))
 app.add_middleware(
     CORSMiddleware,
     allow_origins=origins,
